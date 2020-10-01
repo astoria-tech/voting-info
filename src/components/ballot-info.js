709b6c769d145301978ad0c5221571e0fec4c6a9
Omar De Leo <omar.deleo@gmail.com>
2020-09-30 20:55:43 -0400

    Add Ballet Info widget iframe

--- src/components/ballot-info.js
+++ src/components/ballot-info.js
@@ -7,7 +7,14 @@ const BallotInfo = ({ headline }) => {
   return (
     <div className="ballot-info">
       <h2 className="ballot-info__headline">{headline}</h2>
-      <div className="ballot-info__body"></div>
+      <div className="ballot-info__body">
+        <iframe
+          id="etp-iframe"
+          src="http://onyourballot.vote411.org/build.do"
+          title="Build your personalized ballot"
+          scrolling="no"
+        ></iframe>
+      </div>
     </div>
   )
 }
@@ -19,3 +26,16 @@ BallotInfo.propTypes = {
 }
 
 export default BallotInfo
+
+/*
+  <iframe
+    id="etp-iframe"
+    sandbox="allow-scripts allow-same-origin allow-popups"
+    src="https://onyourballot.vote411.org/build.do?q=34-30+42nd+St+Long+Island+City+New+York+11101&amp;lucky=true"
+    title="Build your personalized ballot"
+    width="100%"
+    scrolling="no"
+    style="overflow: hidden; height: 342px;"
+  >
+  </iframe>
+*/
